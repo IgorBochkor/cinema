8ffebf699414a3c32818e95a833b50aae301c970
Igor Bochkor <igorbochkor@gmail.com>
2021-02-01 17:24:19 +0200

    added solution hb-04

---
+++ src/main/java/com/dev/cinema/dao/impl/MovieSessionDaoImpl.java
@@ -0,0 +1,59 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.MovieSessionDao;
+import com.dev.cinema.exception.DataProcessingException;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.MovieSession;
+import com.dev.cinema.util.HibernateUtil;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class MovieSessionDaoImpl implements MovieSessionDao {
+    @Override
+    public MovieSession add(MovieSession movieSession) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(movieSession);
+            transaction.commit();
+            return movieSession;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert movie session entity "
+                    + movieSession, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        LocalDateTime startOfDay = date.atStartOfDay();
+        LocalDateTime endOfDay = date.atTime(LocalTime.MAX);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<MovieSession> findAvailableSessionsQuery
+                    = session.createQuery("SELECT m FROM MovieSession m "
+                    + "where m.movie.id = :movie_id and m.showTime BETWEEN :start "
+                    + "and :end", MovieSession.class)
+                    .setParameter("movie_id", movieId)
+                    .setParameter("start", startOfDay)
+                    .setParameter("end", endOfDay);
+            return findAvailableSessionsQuery.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find movie session by movieId " + movieId
+                    + " and date " + date, e);
+        }
+    }
+}
